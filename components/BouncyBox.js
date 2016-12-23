9822e60a12f9d5332db2563841351a3842ff6e91
jeff christian <jchri@MacBook-Pro-2.loca>
2016-12-22 23:15:22 -0800

    add long press click event

--- components/BouncyBox.js
+++ components/BouncyBox.js
@@ -4,15 +4,28 @@ import {
   Text,
   View,
   Animated,
-	Image
+	Image,
+  TouchableWithoutFeedback
 } from 'react-native';
+var ACTION_TIMER = 1000;
+var COLORS = ['#FCE4EC', '#F06292'];
 
 export default class BouncyBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       bounceValue: new Animated.Value(0),
+      pressAction: new Animated.Value(0),
+      buttonWidth: 0,
     };
+    this.handlePressIn = this.handlePressIn.bind(this)
+    this.handlePressOut = this.handlePressOut.bind(this)
+    this.getProgressStyles = this.getProgressStyles.bind(this)
+    this.getButtonWidthLayout = this.getButtonWidthLayout.bind(this)
+  }
+  componentWillMount() {
+    this._value = 0;
+    this.state.pressAction.addListener((v) => this._value = v.value)
   }
 	componentDidMount() {
 		var self = this;
@@ -24,40 +37,69 @@ export default class BouncyBox extends React.Component {
 	 }).start()
 	 } , 250 )                             // Start the animation
   }
-	componentDidUnMount() {
-    Animated.timing(this.state.bounceValue,                 // Animate `bounceValue`
-      {
-        toValue: 0,                         // Animate to smaller size
-        duration: this._defaultTransition                          // Bouncier spring
-      }).start();                                // Start the animation
+  handlePressIn() {
+    Animated.timing(this.state.pressAction, {
+      duration: ACTION_TIMER,
+      toValue: 1
+    }).start();
+  }
+  handlePressOut() {
+    Animated.timing(this.state.pressAction, {
+      duration: this._value * ACTION_TIMER,
+      toValue: 0
+    }).start()
+  }
+   getButtonWidthLayout(e) {
+    this.setState({
+        buttonWidth: e.nativeEvent.layout.width - 6,
+        buttonHeight: e.nativeEvent.layout.height - 6
+    });
+  }
+  getProgressStyles() {
+    var width = this.state.pressAction.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, this.state.buttonWidth]
+    })
+    var bgColor = this.state.pressAction.interpolate ({
+      inputRange: [0, 1],
+      outputRange: COLORS
+    })
+    console.log(COLORS[0], COLORS[1])
+    console.log(this.state.buttonWidth, bgColor, width)
+    return {
+      width: this.state.width,
+      height: this.state.height,
+      backgroundColor: bgColor,
+    }
   }
   render() {
 		console.log(this.props.image)
     return (
-				<Animated.View                         // Base: Image, Text, View
-					style={{transform: [                        // `transform` is an ordered array
+      <TouchableWithoutFeedback 
+        style={{width: 177}}
+        onPressIn={this.handlePressIn}
+        onPressOut={this.handlePressOut}
+        >
+         <Animated.View     
+          onLayout={this.getButtonWidthLayout}
+					style={[{ transform: [                        // `transform` is an ordered array
             {scale: this.state.bounceValue},  // Map `bounceValue` to `scale`
-          ]}}
-					>	
-					<View style={styles.shadowBox}>
-						<Image style={{marginTop: 10, width: 177, height: 130}}  source={{uri:`${this.props.image}`}}/>
-						<Text style={{fontSize: 20	}}>{this.props.text}</Text>
-					</View>
-				</Animated.View>
+          ]}, styles.shadowBox, this.getProgressStyles()]}
+					>
+            <Image style={{marginTop: 10, width: 177, height: 130}}  source={{uri:`${this.props.image}`}}/>
+            <Text style={{fontSize: 20, alignSelf: 'center'}}>{this.props.text}</Text>
+          </Animated.View>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
-  shadowBox: {
-		marginTop: 20,
-		alignItems:'center',
+  shadowBox: {    
 		shadowColor: 'black',
-		width: 178,
-		height: 178,
-		backgroundColor: '#FCE4EC',
 		shadowOpacity: 1,
 		shadowOffset: {width: 1, height: 1},
 		shadowRadius: 2,
-  }
+  },
 });
